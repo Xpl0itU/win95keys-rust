4daccaa9db5db01a1590fbe747ca9f2efb7f0d14
Xpl0itU <samuelcarballogarcia8@gmail.com>
2022-07-27 01:30:39 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+extern crate rand;
+
+use rand::distributions::{Distribution, Uniform};
+
+fn sum(mut number: u32) -> u32 {
+    let mut sum_all_nums = 0;
+    while number > 0 {
+        sum_all_nums += number % 10;
+        number = number / 10;
+    }
+    return sum_all_nums;
+}
+
+fn retail() -> String {
+    let mut rng = rand::thread_rng();
+    let first_part_die = Uniform::from(0..998);
+    let second_part_die = Uniform::from(0..9999999);
+    let invalid_nums: [u32; 6] = [333, 444, 555, 666, 777, 888];
+    let mut first_part: u32;
+    let mut second_part: u32;
+
+    loop {
+        first_part = first_part_die.sample(&mut rng);
+        if !invalid_nums.contains(&first_part) {
+            break;
+        }
+    }
+
+    loop {
+        second_part = second_part_die.sample(&mut rng);
+        if (sum(second_part) % 7) == 0 {
+            break;
+        }
+    }
+    return format!("{:03}-{:07}", first_part, second_part);
+}
+
+fn oem() -> String {
+    let mut rng = rand::thread_rng();
+    let first_part_die = Uniform::from(1..366);
+    let second_part_die = Uniform::from(0..9);
+    let third_part_die = Uniform::from(0..999999);
+    let last_part_die = Uniform::from(0..99999);
+    let second_part_array: [u32; 9] = [95, 96, 97, 98, 99, 00, 01, 02, 03];
+    let first_part: u32;
+    let second_part: u32;
+    let mut third_part: u32;
+    let last_part: u32 = last_part_die.sample(&mut rng);
+
+    first_part = first_part_die.sample(&mut rng);
+
+    let second_part_indice: usize = second_part_die.sample(&mut rng);
+    second_part = second_part_array[second_part_indice];
+
+    loop {
+        third_part = third_part_die.sample(&mut rng);
+        if (sum(third_part) % 7) == 0 {
+            break;
+        }
+    }
+
+    return format!(
+        "{:03}{:02}-{:07}-{:05}",
+        first_part, second_part, third_part, last_part
+    );
+}
+
+fn main() {
+    println!("Retail key: {}", retail());
+    println!("OEM key: {}", oem());
+}
